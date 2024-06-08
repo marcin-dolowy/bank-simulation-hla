c0e69fcb5b79680e00ffa1f0311bf12f9c73592c
Marcin Dołowy <marcindolowy00@gmail.com>
2024-06-08 20:04:30 +0200

    add producer federate

---
+++ src/customer_producer_manager/ProducerFederate.java
@@ -0,0 +1,170 @@
+package customer_producer_manager;
+
+import hla.rti1516e.*;
+import hla.rti1516e.encoding.EncoderFactory;
+import hla.rti1516e.encoding.HLAinteger32BE;
+import hla.rti1516e.exceptions.FederatesCurrentlyJoined;
+import hla.rti1516e.exceptions.FederationExecutionAlreadyExists;
+import hla.rti1516e.exceptions.FederationExecutionDoesNotExist;
+import hla.rti1516e.exceptions.RTIexception;
+import hla.rti1516e.time.HLAfloat64Interval;
+import hla.rti1516e.time.HLAfloat64Time;
+import hla.rti1516e.time.HLAfloat64TimeFactory;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class ProducerFederate {
+    public static final String READY_TO_RUN = "ReadyToRun";
+    
+    private RTIambassador rtiamb;
+    private ProducerFederateAmbassador fedamb;  // created when we connect
+    private HLAfloat64TimeFactory timeFactory; // set when we join
+    protected EncoderFactory encoderFactory;     // set when we join
+
+    protected InteractionClassHandle addProductsHandle;
+    
+
+    private void log(String message) {
+        System.out.println("ProducerFederate   : " + message);
+    }
+    
+    private void waitForUser() {
+        log(" >>>>>>>>>> Press Enter to Continue <<<<<<<<<<");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            reader.readLine();
+        } catch (Exception e) {
+            log("Error while waiting for user input: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public void runFederate(String federateName) throws Exception {
+        log("Creating RTIambassador");
+        rtiamb = RtiFactoryFactory.getRtiFactory().getRtiAmbassador();
+        encoderFactory = RtiFactoryFactory.getRtiFactory().getEncoderFactory();
+
+        // connect
+        log("Connecting...");
+        fedamb = new ProducerFederateAmbassador(this);
+        rtiamb.connect(fedamb, CallbackModel.HLA_EVOKED);
+
+        log("Creating Federation...");
+        try {
+            URL[] modules = new URL[]{
+                    (new File("foms/ProducerConsumer.xml")).toURI().toURL(),
+            };
+
+            rtiamb.createFederationExecution("ProducerConsumerFederation", modules);
+            log("Created Federation");
+        } catch (FederationExecutionAlreadyExists exists) {
+            log("Didn't create federation, it already existed");
+        } catch (MalformedURLException urle) {
+            log("Exception loading one of the FOM modules from disk: " + urle.getMessage());
+            urle.printStackTrace();
+            return;
+        }
+
+        rtiamb.joinFederationExecution(federateName,            // name for the federate
+                "producer",   // federate type
+                "ProducerConsumerFederation"     // name of federation
+        );
+
+        log("Joined Federation as " + federateName);
+
+        // cache the time factory for easy access
+        this.timeFactory = (HLAfloat64TimeFactory) rtiamb.getTimeFactory();
+
+        rtiamb.registerFederationSynchronizationPoint(READY_TO_RUN, null);
+        // wait until the point is announced
+        while (fedamb.isAnnounced == false) {
+            rtiamb.evokeMultipleCallbacks(0.1, 0.2);
+        }
+
+        waitForUser();
+
+        rtiamb.synchronizationPointAchieved(READY_TO_RUN);
+        log("Achieved sync point: " + READY_TO_RUN + ", waiting for federation...");
+        while (fedamb.isReadyToRun == false) {
+            rtiamb.evokeMultipleCallbacks(0.1, 0.2);
+        }
+
+        enableTimePolicy();
+        log("Time Policy Enabled");
+
+        publishAndSubscribe();
+        log("Published and Subscribed");
+
+        Producer producer = new Producer();
+        while (fedamb.isRunning) {
+            int producedCustomerId = producer.produce();
+            ParameterHandleValueMap parameterHandleValueMap = rtiamb.getParameterHandleValueMapFactory().create(1);
+            ParameterHandle addCustomerIdHandle = rtiamb.getParameterHandle(addProductsHandle, "customerId");
+            HLAinteger32BE count = encoderFactory.createHLAinteger32BE(producedCustomerId);
+            parameterHandleValueMap.put(addCustomerIdHandle, count.toByteArray());
+            rtiamb.sendInteraction(addProductsHandle, parameterHandleValueMap, generateTag());
+
+            advanceTime(producer.getTimeToNext());
+            log("Time Advanced to " + fedamb.federateTime);
+        }
+
+        rtiamb.resignFederationExecution(ResignAction.DELETE_OBJECTS);
+        log("Resigned from Federation");
+
+        try {
+            rtiamb.destroyFederationExecution("ExampleFederation");
+            log("Destroyed Federation");
+        } catch (FederationExecutionDoesNotExist dne) {
+            log("No need to destroy federation, it doesn't exist");
+        } catch (FederatesCurrentlyJoined fcj) {
+            log("Didn't destroy federation, federates still joined");
+        }
+    }
+
+    private void enableTimePolicy() throws Exception {
+        HLAfloat64Interval lookahead = timeFactory.makeInterval(fedamb.federateLookahead);
+        this.rtiamb.enableTimeRegulation(lookahead);
+        while (fedamb.isRegulating == false) {
+            rtiamb.evokeMultipleCallbacks(0.1, 0.2);
+        }
+        this.rtiamb.enableTimeConstrained();
+        while (fedamb.isConstrained == false) {
+            rtiamb.evokeMultipleCallbacks(0.1, 0.2);
+        }
+    }
+
+    private void publishAndSubscribe() throws RTIexception {
+        String iname = "HLAinteractionRoot.addCustomer";
+        addProductsHandle = rtiamb.getInteractionClassHandle(iname);
+        rtiamb.publishInteractionClass(addProductsHandle);
+    }
+
+    private void advanceTime(double timestep) throws RTIexception {
+        fedamb.isAdvancing = true;
+        HLAfloat64Time time = timeFactory.makeTime(fedamb.federateTime + timestep);
+        rtiamb.timeAdvanceRequest(time);
+        while (fedamb.isAdvancing) {
+            rtiamb.evokeMultipleCallbacks(0.1, 0.2);
+        }
+    }
+
+    private byte[] generateTag() {
+        return ("(timestamp) " + System.currentTimeMillis()).getBytes();
+    }
+
+    public static void main(String[] args) {
+        String federateName = "CustomerProducer";
+        if (args.length != 0) {
+            federateName = args[0];
+        }
+        try {
+            new ProducerFederate().runFederate(federateName);
+        } catch (Exception rtie) {
+            rtie.printStackTrace();
+        }
+    }
+}
---
+++ src/customer_producer_manager/ProducerFederateAmbassador.java
@@ -0,0 +1,156 @@
+package customer_producer_manager;
+
+import hla.rti1516e.*;
+import hla.rti1516e.encoding.DecoderException;
+import hla.rti1516e.encoding.HLAinteger32BE;
+import hla.rti1516e.exceptions.FederateInternalError;
+import hla.rti1516e.time.HLAfloat64Time;
+import org.portico.impl.hla1516e.types.encoding.HLA1516eInteger32BE;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class ProducerFederateAmbassador extends NullFederateAmbassador {
+    private ProducerFederate federate;
+
+    // these variables are accessible in the package
+    protected double federateTime = 0.0;
+    protected double federateLookahead = 1.0;
+
+    protected boolean isRegulating = false;
+    protected boolean isConstrained = false;
+    protected boolean isAdvancing = false;
+
+    protected boolean isAnnounced = false;
+    protected boolean isReadyToRun = false;
+    protected boolean isRunning = true;
+
+    public ProducerFederateAmbassador(ProducerFederate federate) {
+        this.federate = federate;
+    }
+
+    private void log(String message) {
+        System.out.println("FederateAmbassador: " + message);
+    }
+
+    @Override
+    public void synchronizationPointRegistrationFailed(String label,
+                                                       SynchronizationPointFailureReason reason) {
+        log("Failed to register sync point: " + label + ", reason=" + reason);
+    }
+
+    @Override
+    public void synchronizationPointRegistrationSucceeded(String label) {
+        log("Successfully registered sync point: " + label);
+    }
+
+    @Override
+    public void announceSynchronizationPoint(String label, byte[] tag) {
+        log("Synchronization point announced: " + label);
+        if (label.equals(ProducerFederate.READY_TO_RUN))
+            this.isAnnounced = true;
+    }
+
+    @Override
+    public void federationSynchronized(String label, FederateHandleSet failed) {
+        log("Federation Synchronized: " + label);
+        if (label.equals(ProducerFederate.READY_TO_RUN))
+            this.isReadyToRun = true;
+    }
+
+    @Override
+    public void timeRegulationEnabled(LogicalTime time) {
+        this.federateTime = ((HLAfloat64Time) time).getValue();
+        this.isRegulating = true;
+    }
+
+    @Override
+    public void timeConstrainedEnabled(LogicalTime time) {
+        this.federateTime = ((HLAfloat64Time) time).getValue();
+        this.isConstrained = true;
+    }
+
+    @Override
+    public void timeAdvanceGrant(LogicalTime time) {
+        this.federateTime = ((HLAfloat64Time) time).getValue();
+        this.isAdvancing = false;
+    }
+
+    @Override
+    public void discoverObjectInstance(ObjectInstanceHandle theObject,
+                                       ObjectClassHandle theObjectClass,
+                                       String objectName)
+            throws FederateInternalError {
+        log("Discoverd Object: handle=" + theObject + ", classHandle=" +
+                theObjectClass + ", name=" + objectName);
+    }
+
+    @Override
+    public void reflectAttributeValues(ObjectInstanceHandle theObject,
+                                       AttributeHandleValueMap theAttributes,
+                                       byte[] tag,
+                                       OrderType sentOrder,
+                                       TransportationTypeHandle transport,
+                                       SupplementalReflectInfo reflectInfo)
+            throws FederateInternalError {
+        reflectAttributeValues(theObject,
+                theAttributes,
+                tag,
+                sentOrder,
+                transport,
+                null,
+                sentOrder,
+                reflectInfo);
+    }
+
+    @Override
+    public void reflectAttributeValues(ObjectInstanceHandle theObject,
+                                       AttributeHandleValueMap theAttributes,
+                                       byte[] tag,
+                                       OrderType sentOrdering,
+                                       TransportationTypeHandle theTransport,
+                                       LogicalTime time,
+                                       OrderType receivedOrdering,
+                                       SupplementalReflectInfo reflectInfo)
+            throws FederateInternalError {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public void receiveInteraction(InteractionClassHandle interactionClass,
+                                   ParameterHandleValueMap theParameters,
+                                   byte[] tag,
+                                   OrderType sentOrdering,
+                                   TransportationTypeHandle theTransport,
+                                   SupplementalReceiveInfo receiveInfo)
+            throws FederateInternalError {
+        this.receiveInteraction(interactionClass,
+                theParameters,
+                tag,
+                sentOrdering,
+                theTransport,
+                null,
+                sentOrdering,
+                receiveInfo);
+    }
+
+    @Override
+    public void receiveInteraction(InteractionClassHandle interactionClass,
+                                   ParameterHandleValueMap theParameters,
+                                   byte[] tag,
+                                   OrderType sentOrdering,
+                                   TransportationTypeHandle theTransport,
+                                   LogicalTime time,
+                                   OrderType receivedOrdering,
+                                   SupplementalReceiveInfo receiveInfo)
+            throws FederateInternalError {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public void removeObjectInstance(ObjectInstanceHandle theObject,
+                                     byte[] tag,
+                                     OrderType sentOrdering,
+                                     SupplementalRemoveInfo removeInfo)
+            throws FederateInternalError {
+        log("Object Removed: handle=" + theObject);
+    }
+}
